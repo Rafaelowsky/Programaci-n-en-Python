0f40f63d501d830732f0099eb346c795ca90fdac
Rafaelowsky <319055539@cuautitlan.unam.mx>
2023-07-01 12:51:40 -0600

    Agregué comentarios

--- Curso Profesional Python/listas.py
+++ Curso Profesional Python/listas.py
@@ -4,11 +4,13 @@ lista = [] #Lista vacia
 
 #Las listas pueden guardar varios tipos de datos
 #          0      1     2     3       4
+#          -5    -4    -3    -2      -1
 lista = ['Hola', True, 4.5, False, 'Mundo']
 
 """En Python los indices empiezan desde 0
 Para poder acceder a un elemento de la lista se hace de la siguiente manera: """
 print(lista[0]) #Hola
+print(lista[-5]) #Hola
 
 #Por convención las listas deben contener datos del mismo tipo
 
