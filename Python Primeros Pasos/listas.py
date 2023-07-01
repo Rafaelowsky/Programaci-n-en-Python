b528ae6233f746add09be3a61c0b56875b1e1877
Rafaelowsky <319055539@cuautitlan.unam.mx>
2023-06-30 22:40:21 -0600

    Cree listas.py

---
+++ Python Primeros Pasos/listas.py
@@ -0,0 +1,24 @@
+#Para poder declarar una lista en Python se hace de la siguiente manera:
+
+lista = [] #Lista vacia
+
+"""Las listas pueden guardar varios tipos de datos
+En Python los indices empiezan desde 0
+Para poder acceder a un elemento de la lista se hace de la siguiente manera:"""
+
+lista = ['Hola', True, 4.5, False, 'Mundo']
+print(lista[0]) #Hola
+
+#Por convención las listas deben contener datos del mismo tipo
+
+lista = [1,2,3,4,5,6,7,8,9,10]
+
+lista = [True,True,False,True]
+
+lista = ["Hola","Mundo","Python"]
+
+
+
+
+
+
