8a82f5b7f8d2c28755c6cedc9ddca0a7f25ea232
Rafaelowsky <319055539@cuautitlan.unam.mx>
2023-06-30 21:05:39 -0600

    Añadí función para saber los tipos de datos

--- Python Primeros Pasos/tiposDeDatos.py
+++ Python Primeros Pasos/tiposDeDatos.py
@@ -3,6 +3,9 @@
 titulo_curso = "Curso profesional de Python"
 print(titulo_curso)
 
+#Type nos dice el tipo de dato que es
+print(type(titulo_curso)) #<class 'str'>
+
 nombre_completo = 'Rafael Zavala Saldivar'
 print(nombre_completo)
 
@@ -17,6 +20,7 @@ print(mensaje)
 
 numero_estudiantes = 100
 print(numero_estudiantes)
+print(type(numero_estudiantes)) #<class 'int'>
 
 #Al dividir con / nos da un numero flotante
 numero_estudiantes = 100/3 
@@ -30,9 +34,11 @@ print(numero_estudiantes)
 
 calificacion = 4.5
 print(calificacion)
+print(type(calificacion)) #<class 'float'>
 
 #Boolean = verdadero o falso
 
 aprobado = True
 print(aprobado)
+print(type(aprobado)) #<class 'bool'>
 
