7710e0f49963972f38628b7a30c27bffed3d27ac
Rafaelowsky <319055539@cuautitlan.unam.mx>
2023-06-30 20:55:25 -0600

    Añadí contenido logicos.py

--- Python Primeros Pasos/logicos.py
+++ Python Primeros Pasos/logicos.py
@@ -1,1 +1,35 @@
+#Los operadores lógicos son los siguientes:
+
+# and: Devuelve True si ambos operandos son True
+
+ejemplo = True and True
+print(ejemplo) #True
+
+# or: Devuelve True si al menos uno de los operandos es True
+
+ejemplo = True or False
+print(ejemplo) #True
+
+# not: Devuelve True si el operando es False y viceversa
+
+ejemplo = not False
+print(ejemplo) #True
+
+# Ejemplo de uso de operadores lógicos
+
+numero1 = 5
+numero2 = 10
+
+# and
+
+print(numero1 < 10 and numero2 > 5) #True
+
+# or
+
+print(numero1 < 10 or numero2 < 5) #True
+
+# not
+
+print(not numero1 < 10) #False
+
 
