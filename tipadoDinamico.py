08a99c7ce0086df42a68508a73e054b2080510bf
Rafaelowsky <319055539@cuautitlan.unam.mx>
2023-06-30 20:26:54 -0600

    Añadí tipadoDinamico.py

---
+++ tipadoDinamico.py
@@ -0,0 +1,23 @@
+#Una variable puede contener cualquier tipo de dato
+#y puede cambiar de tipo en cualquier momento
+
+ejemplo = 'Hola'
+
+print(ejemplo)
+
+ejemplo = 10
+
+print(ejemplo)
+
+ejemplo = 10.5
+
+print(ejemplo)
+
+ejemplo = True
+
+print(ejemplo)
+
+ejemplo = None
+
+print(ejemplo)
+
