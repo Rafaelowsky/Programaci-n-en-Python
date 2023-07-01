3b3a5a7c486624880dc91726513bfdec185d49a8
Rafaelowsky <319055539@cuautitlan.unam.mx>
2023-06-30 20:38:06 -0600

    Añadí relacionales.py

---
+++ relacionales.py
@@ -0,0 +1,22 @@
+numero1 = 5
+numero2 = 10
+
+#Los operadores relacionales son los siguientes:
+
+# == Igual que
+print(numero1 == numero2) #False
+
+# != Distinto que
+print(numero1 != numero2) #True
+
+# < Menor que
+print(numero1 < numero2) #True
+
+# > Mayor que
+print(numero1 > numero2) #False
+
+# <= Menor o igual que
+print(numero1 <= numero2) #True
+
+# >= Mayor o igual que
+print(numero1 >= numero2) #False
