71ed33fbabe9dda00b7cee6438a365026a528f4d
Rafaelowsky <319055539@cuautitlan.unam.mx>
2023-06-30 20:10:59 -0600

    Añadí tiposDeDatos.py

---
+++ tiposDeDatos.py
@@ -0,0 +1,38 @@
+#String = cadena de caracteres
+
+titulo_curso = "Curso profesional de Python"
+print(titulo_curso)
+
+nombre_completo = 'Rafael Zavala Saldivar'
+print(nombre_completo)
+
+mensaje= "'Código facilito'"
+print(mensaje)
+
+mensaje = """Estas en el curso de python
+por parte de Codigofacilito """
+print(mensaje)
+
+#Integer = numero entero
+
+numero_estudiantes = 100
+print(numero_estudiantes)
+
+#Al dividir con / nos da un numero flotante
+numero_estudiantes = 100/3 
+print(numero_estudiantes)
+
+#Al dividir con // nos da un numero entero
+numero_estudiantes = 100//3
+print(numero_estudiantes)
+
+#Float = numero decimal
+
+calificacion = 4.5
+print(calificacion)
+
+#Boolean = verdadero o falso
+
+aprobado = True
+print(aprobado)
+
