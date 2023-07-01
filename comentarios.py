435e37eec76828df3e953d5dc5d9372b7dc47a0a
Rafaelowsky <319055539@cuautitlan.unam.mx>
2023-06-30 19:44:39 -0600

    Añadí comentarios.py

---
+++ comentarios.py
@@ -0,0 +1,7 @@
+# Con este simbolo se puede comentar una linea
+
+print("Hola mundo")
+
+""" se ocupan las triples comillas para un comentario
+de varias lineas si es que se necesita
+ comentar varias lineas, se puede usar tambien el simbolo de gato"""
