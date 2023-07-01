29eff67f3ceb435a62fe32c5c886a04becf9a341
Rafaelowsky <319055539@cuautitlan.unam.mx>
2023-06-30 18:52:22 -0600

    Agregué variables.py

---
+++ variables.py
@@ -0,0 +1,6 @@
+#El codigo siempre es secuencial, es decir, se ejecuta de arriba hacia abajo
+titulo_curso = "Curso Profesional de Python" # Declaramos una variable
+
+nombre_completo = "Rafael Zavala Saldivar" # Declaramos una variable 
+
+print(titulo_curso) # Imprimimos el valor de la variable titulo_curso
