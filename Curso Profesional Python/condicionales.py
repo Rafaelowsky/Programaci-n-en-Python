b83cf2d143f4097e83b2c600bb7d03a97f0eaa28
Rafaelowsky <319055539@cuautitlan.unam.mx>
2023-07-02 14:56:58 -0600

    Creé condicionales.py

---
+++ Curso Profesional Python/condicionales.py
@@ -0,0 +1,11 @@
+resultado = int (input (' Ingresa un valor: '))
+
+# Las condicionales nos permiten evaluar si una expresión es verdadera o falsa
+# dependiendo de su valor ejecutaremos cierto código
+
+if resultado > 10 and resultado < 20:
+    print ("El valor cumple con la condición")
+
+# Si la condición no se cumple podemos ejecutar otro código
+else:
+    print ("El valor no cumple con la condición")
