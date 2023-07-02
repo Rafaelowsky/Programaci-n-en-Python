32d968e70f2482f391b1d91efe7161f3214a4fc8
Rafaelowsky <319055539@cuautitlan.unam.mx>
2023-07-01 22:15:42 -0600

    Quite el desempaquetado

--- Curso Profesional Python/tuplas.py
+++ Curso Profesional Python/tuplas.py
@@ -22,17 +22,3 @@ niveles = ["Basico", "Intermedio", "Avanzado"]
 tupla_niveles = tuple(niveles)
 
 print(tupla_niveles) #('Basico', 'Intermedio', 'Avanzado')
-
-#Así podemos desempaquetar elementos de una tupla
-
-tupla = (1,2,3,4,5,6,7,8,9,10)
-
-# * para la creación de una lista
-# _ para ignorar un elemento
-# *_ para ignorar varios elementos
-uno, _, tres, _, *elementos_restantes = tupla
-
-print (uno) #1
-print (elementos_restantes) #[5, 6, 7, 8, 9, 10]
-
-#Solo se permite un solo * en la desempaquetación 
