7cc4a9ad7ebb43bc3ab56d4e8084324dcd65aaee
Rafaelowsky <319055539@cuautitlan.unam.mx>
2023-07-02 15:06:12 -0600

    Añadí condicional elif

--- Curso Profesional Python/condicionales.py
+++ Curso Profesional Python/condicionales.py
@@ -9,3 +9,16 @@ if resultado > 10 and resultado < 20:
 # Si la condición no se cumple podemos ejecutar otro código
 else:
     print ("El valor no cumple con la condición")
+
+# Tambien podemos usar elif para evaluar otra condición
+
+calificacion = 8 
+
+if calificacion == 10:
+    print ("Felicidades, tu calificación es excelente")
+
+elif calificacion >= 6:
+    print ("Felicidades, aprobaste el curso")
+
+else:
+    print ("Reprobaste el curso")
