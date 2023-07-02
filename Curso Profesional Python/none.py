2a33c4ad06504afdee86b85b6134acd12c95cc58
Rafaelowsky <319055539@cuautitlan.unam.mx>
2023-07-02 14:41:07 -0600

    Agregué como se representa false

---
+++ Curso Profesional Python/none.py
@@ -0,0 +1,28 @@
+# Si queremos inicializar una variable que no tenga tipo podemos hacer lo siguiente:
+
+resultado = None
+
+print(resultado) # None
+
+# None es un tipo de dato que representa la ausencia de valor, es decir, que no tiene ningún valor asignado
+# Pero podemos asignarle un valor despues sin que haya problemas
+
+resultado = [1, 2, 3]
+
+print(resultado) # [1, 2, 3]
+
+# True = 1 / False = 0
+
+# El booleano false se puede representar de las siguientes formas:
+
+"""
+False
+None
+0
+0.0
+'' / ""
+[]
+()
+{}
+"""
+
