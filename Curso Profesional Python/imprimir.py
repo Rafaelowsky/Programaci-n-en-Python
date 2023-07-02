18509dc112d4f51b244ff19154be8b0c0d4b88bf
Rafaelowsky <319055539@cuautitlan.unam.mx>
2023-07-02 10:58:06 -0600

    Creé imprimir.py

---
+++ Curso Profesional Python/imprimir.py
@@ -0,0 +1,7 @@
+#El método print() es una función incorporada de Python que envía datos a la secuencia de salida estándar (pantalla).
+
+nombre = "Rafael"
+apellido = "Zavala"
+
+#sep es el separador entre los argumentos que se imprimen, por defecto es un espacio en blanco.
+print ("Mr. ", nombre, " ", apellido, ".", (1,2,3,4,5), sep="")
